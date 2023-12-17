4720277a2970812d62769b8586891ea393bb5b95
sangam0207 <127228515+sangam0207@users.noreply.github.com>
2023-12-18 03:47:39 +0530

    Delete server directory

--- server/db/connection.js
+++
@@ -1,8 +0,0 @@
-const mongoose=require('mongoose');
-mongoose.connect('mongodb://localhost:27017/MernLogin')
-.then(()=>{
-    console.log('connection successfully');
-})
-.catch((error)=>{
-   console.log('Connection Failed')
-})
